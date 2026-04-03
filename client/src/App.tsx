00e6ba9eb54692518f8bf547c9581d1b80fcc47d
pravin435916 <pravinnandankar03@gmail.com>
2026-04-03 19:00:17 +0530

    feat:added route and empty folders

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+const Home = () => <div>Home Page</div>
+const About = () => <div>About Page</div>
+const Contact = () => <div>Contact Page</div>
 
 const App = () => {
   return (
-    <div className='text-orange-800'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
