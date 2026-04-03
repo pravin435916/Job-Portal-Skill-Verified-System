163b45c122cc8cae911e9ea69830c70869e4c3c0
Sanika <sanika@codewithesanika.com>
2026-04-03 16:55:30 +0530

    my changes

--- server/app/main.py
+++ server/app/main.py
@@ -4,13 +4,14 @@ from app.core.seed import seed_data
 from app.core.db import init_db
 
 app = FastAPI(title="Skill Verified Job Portal")
+app.include_router(api_router, prefix="/api/v1")
 
 @app.on_event("startup")
 async def on_startup():
     await init_db()
 
 
-app.include_router(api_router, prefix="/api/v1")
+
 
 @app.get("/")
 def root():
