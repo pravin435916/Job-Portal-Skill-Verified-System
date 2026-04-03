35a5bf379ec7245e8b7a852e05a3276e28b71215
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2026-04-03 11:26:47 +0530

    db

--- server/app/core/db.py
+++ server/app/core/db.py
@@ -3,7 +3,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 import os
 from dotenv import load_dotenv
-from app.models.candidate import Candidate
+# from app.models.candidate import Candidate
 
 load_dotenv()
 
