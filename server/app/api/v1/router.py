96227d963064612d4abc4ea48851b5bff1ee4a33
pravin435916 <pravinnandankar03@gmail.com>
2026-04-03 16:49:04 +0530

    fix : job routes

--- server/app/api/v1/router.py
+++ server/app/api/v1/router.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import applications, candidate, matching
+from app.api.v1.endpoints import applications, candidate, matching , jobs
 
 api_router = APIRouter()
 
 api_router.include_router(matching.router, tags=["Matching"])
 api_router.include_router(applications.router, prefix="/applications", tags=["Applications"])
 api_router.include_router(candidate.router, prefix="/candidate", tags=["Candidate"])
+api_router.include_router(jobs.router, prefix="/jobs", tags=["Jobs"])
